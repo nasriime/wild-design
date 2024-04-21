3867bf0044f0219037810d9093ed5f26d439ced3
nasriime <mnasr4040@gmail.com>
2024-04-21 16:07:43 +0200

    images arrayes

---
+++ src/images.ts
@@ -0,0 +1,13 @@
+import image01 from './images/image01.jpg';
+import image01_2x from './images/image01@2x.jpg';
+import image02 from './images/image01.jpg';
+import image02_2x from './images/image01@2x.jpg';
+import image03 from './images/image01.jpg';
+import image03_2x from './images/image01@2x.jpg';
+import image04 from './images/image01.jpg';
+import image04_2x from './images/image01@2x.jpg';
+import image05 from './images/image01.jpg';
+import image05_2x from './images/image01@2x.jpg';
+
+export const smallImages = [image01, image02, image03, image04, image05];
+export const largeImages = [image01_2x, image02_2x, image03_2x, image04_2x, image05_2x];
