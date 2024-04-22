c6f249f5f820e58cd57801d4fc7aacee8a8e734a
nasriime <mnasr4040@gmail.com>
2024-04-22 12:33:13 +0200

    add scrolling logic

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,31 +1,52 @@
-import { useState, useEffect, useRef} from 'react';
-import { motion } from 'framer-motion';
+import { useState, useLayoutEffect, useCallback, useRef} from 'react';
+import { motion, useViewportScroll, useTransform, useSpring } from 'framer-motion';
 import SingleSlide from './SingleSlide';
 import './Home.css';
 import { smallImages } from './sliderContent';
 function Home() {
-  const [width, setWidth] = useState(0);
-  const carousel = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const ghostRef = useRef(null)
+  const [scrollRange, setScrollRange] = useState(0)
+  const [viewportW, setViewportW] = useState(0)
 
-  useEffect(() => {
-    if (carousel.current && carousel.current.offsetParent) {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetParent.clientWidth);
+  useLayoutEffect(() => {
+    scrollRef && scrollRef.current && setScrollRange(scrollRef.current.scrollWidth)
+  }, [scrollRef])
+
+  const onResize = useCallback((entries: ResizeObserverEntry[]) => {
+    for (let entry of entries) {
+      setViewportW(entry.contentRect.width)
     }
   }, [])
 
+  useLayoutEffect(() => {
+    const resizeObserver = new ResizeObserver(entries => onResize(entries))
+    ghostRef.current && resizeObserver.observe(ghostRef.current)
+    return () => resizeObserver.disconnect()
+  }, [onResize])
+
+  const { scrollYProgress } = useViewportScroll()
+  const transform = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, -scrollRange + viewportW]
+  )
+  const physics = { damping: 15, mass: 0.27, stiffness: 55 }
+  const spring = useSpring(transform, physics)
+
   return (
     <div className="">
-      <motion.div className="cursor-grab overflow-hidden">
+      <motion.div className="fixed overflow-hidden">
         <motion.div 
-          ref={carousel} 
-          drag="x" 
-          dragConstraints={{ right: 0, left: -width }}
+          ref={scrollRef} 
+          style={{ x: spring }}
           className="w-max flex">
             {smallImages.map((image, index) => (
              <SingleSlide key={index} image={image} index={index}/>
             ))}
         </motion.div>
       </motion.div>
+      <div ref={ghostRef} style={{ height: scrollRange }} className="ghost" />
     </div>
   );
 }
--- src/SingleSlide.tsx
+++ src/SingleSlide.tsx
@@ -21,41 +21,41 @@ function SingleSlide({image, index}: {image: string, index: number}) {
                 <p className="text-white absolute top-[16px] left-[16px] uppercase">XYZ Photography</p>
                 
                 <div className="absolute overflow-hidden transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                <img 
-                    className="w-[512px] h-[680px] rounded-[10px] border-[1px] border-black pointer-events-none" 
-                    src={image} 
-                    alt="" />
-                <div className="text-white text-center absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                    <p className="font-bold text-[150px] uppercase">
-                    {sildersText[index]}
-                    </p>
-                    <p>{index+1} out of {smallImages.length}</p>
-                </div>
+                    <img 
+                        className="w-[512px] h-[680px] rounded-[10px] border-[1px] border-black" 
+                        src={image} 
+                        alt="" />
+                    <div className="text-white text-center absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                        <p className="font-bold text-[150px] uppercase">
+                        {sildersText[index]}
+                        </p>
+                        <p>{index+1} out of {smallImages.length}</p>
+                    </div>
                 </div>
                 {/* Text overlap*/}
                 <div className="text-center absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                <p 
-                    className="text-shadow font-bold text-[150px] text-shadow[] uppercase">
-                    {sildersText[index]}
-                </p>
-                <p className="opacity-0">{index+1} out of {smallImages.length}</p>
+                    <p 
+                        className="text-shadow font-bold text-[150px] text-shadow[] uppercase">
+                        {sildersText[index]}
+                    </p>
+                    <p className="opacity-0">{index+1} out of {smallImages.length}</p>
                 </div>
 
                 <img 
-                className="absolute top-[16px] right-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black pointer-events-none" 
-                src={largeImages.length-1 === index ? `${smallImages[0]}` : `${smallImages[index+1]}`}
-                alt="" />
+                    className="absolute top-[16px] right-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black cursor-pointer" 
+                    src={largeImages.length-1 === index ? `${smallImages[0]}` : `${smallImages[index+1]}`}
+                    alt="" />
 
                 <img 
-                className="absolute bottom-[16px] left-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black pointer-events-none" 
-                src={index === 0 ? `${smallImages[smallImages.length-1]}` : `${smallImages[index-1]}`} 
-                alt="" />
+                    className="absolute bottom-[16px] left-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black cursor-pointer" 
+                    src={index === 0 ? `${smallImages[smallImages.length-1]}` : `${smallImages[index-1]}`} 
+                    alt="" />
 
                 <div className="absolute bottom-[16px] right-[16px]">
                     <p className="text-white uppercase">Johanna Hobel for CHanel</p>
                     <p className="text-white text-right">FEB 2020</p>
                     <button className="text-black rounded-[24px] bg-white px-[16px] pt-[9px] pb-[8px] uppercase">
-                    have a look
+                        have a look
                     </button>
                 </div>
         </motion.div>
