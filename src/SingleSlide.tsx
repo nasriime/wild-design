fd4548bf953956101499e627f5db810b3686278e
nasriime <mnasr4040@gmail.com>
2024-04-25 09:55:23 +0200

    add transation to navigation images and button

--- src/SingleSlide.tsx
+++ src/SingleSlide.tsx
@@ -39,24 +39,24 @@ function SingleSlide({ image, index, swiper }: SingleSlideProps) {
       {/* Text overlap for title*/}
       <MainTitle index={index} shadow={true} opacity={true} swiper={swiper} />
       <img
-        className="absolute top-[16px] right-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black cursor-pointer hover:scale-105"
+        className="absolute top-[16px] right-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] hover:border-[1px] border-black hover:border-white cursor-pointer hover:scale-105 transition-all duration-500 ease-in-out"
         src={images[next]}
         onClick={() => {
           swiper?.slideTo(next);
         }}
         loading={index === 0 ? "eager" : "lazy"}
-        alt={images[next]}
+        alt="Next slide"
       />
       <img
-        className="absolute bottom-[16px] left-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] border-black cursor-pointer hover:scale-105"
+        className="absolute bottom-[16px] left-[16px] w-[248px] h-[330px] rounded-[10px] border-[1px] hover:border-[1px] border-black hover:border-white cursor-pointer hover:scale-105 transition-all duration-500 ease-in-out"
         src={images[prev]}
         onClick={() => {
           swiper?.slideTo(prev);
         }}
         loading={index === 0 ? "eager" : "lazy"}
-        alt={images[prev]}
+        alt="Previous slide"
       />
-      <div className="absolute w-[109px] h-[97px] bottom-[16px] right-[140px] text-[10px] font-helvetica font-normal ">
+      <div className="absolute w-[111px] h-[120px] bottom-[100px] right-[150px] text-[10px] font-helvetica font-normal tracking-[1px]">
         <p className="text-white uppercase leading-3 mb-4 ">
           {sildersContent[index]["author"]}
         </p>
@@ -64,12 +64,12 @@ function SingleSlide({ image, index, swiper }: SingleSlideProps) {
           {sildersContent[index]["date"]}
         </p>
         {/* Ideally this should be a link <a> that opens in a new tab but linter shows warning for emapty href="" */}
-        <button className="text-black hover:text-white bg-white hover:bg-black font-helvetica font-normal rounded-[24px] px-[16px] pt-[9px] pb-[8px] uppercase cursor-pointer leading-3">
+        <button className="text-black hover:text-white bg-white hover:bg-black font-helvetica font-bold rounded-[24px] px-[16px] pt-[9px] pb-[8px] uppercase cursor-pointer leading-3 transition-all duration-400 ease-in-out">
           have a look
         </button>
       </div>
     </div>
-  );
+  )
 }
 
 export default SingleSlide;
